d50c20139de0dbe23aa6b6c3b66c2448f10ac3ef
Renata Brito <renata.oliveira.brito@gmail.com>
2018-02-04 20:31:22 -0200

    carrosel e sessoes

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
+
+// animação nav bar
+
+
+// nav change the color by move the scroll
+
 $(window).on('scroll', function(){
     if($(window).scrollTop()){
         $('nav').addClass('black');
@@ -5,3 +11,22 @@ $(window).on('scroll', function(){
         $('nav').removeClass('black');
     }
 })
+
+// carousel
+
+    let imgs = document.querySelectorAll('.imgJs');
+    let slider = 0;
+
+    function avancarSlide(){
+    imgs[slider].style.display = 'none';
+    slider++;
+
+    if(slider >= imgs.length){
+        slider = 0;
+    }
+
+    imgs[slider].style.display = 'block';
+    }
+
+    setInterval(avancarSlide, 5000);
+
